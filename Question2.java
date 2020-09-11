41dc3d326611ca8e96c01c4591b276390317554c
benjaminth2 <31826810+benjaminth2@users.noreply.github.com>
2020-09-12 00:16:20 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
-/**
- * @author (Cheung Tin Ho Benjamin, 19073365D)
- * Question 2
- **/
 public class Question2
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of seconds: ");
-        int UserInput = scanner.nextInt();  // Read user input
+        int UserInput = scanner.nextInt();
         System.out.println(UserInput + " seconds is " + UserInput/3600 + " hours, " + UserInput%3600/60 + " minutes, and " + (UserInput%3600%60) + " seconds.");
     }
 }
